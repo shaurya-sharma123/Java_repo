9e8d89f5c50d97d32592bf7ca0a733aaa294d148
shaurya-sharma123 <shaurya.s.hp@gmail.com>
2026-02-16 09:00:41 +0530

    UC1 Solution

---
+++ OOPSBannerApp.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class OOPSBannerApp
+{
+    public static void main(String[] args) {
+        {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("OOPS");
+        }
+    }
+}
