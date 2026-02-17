700eddbdabf45ac4b759c3fa39b63cf00d43fe95
shaurya-sharma123 <shaurya.s.hp@gmail.com>
2026-02-17 22:00:40 +0530

    OOPSBannerApp.java was added

---
+++ OOPSBannerApp.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class OOPSBannerApp{
+	public static void main (String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println(" ***** "+" "+" ***** "+" "+" ****** "+" "+" ***** ");
+		System.out.println("*     *"+" "+"*     *"+" "+"*      *"+" "+"*     *");
+		System.out.println("*     *"+" "+"*     *"+" "+"*      *"+" "+"*      ");
+		System.out.println("*     *"+" "+"*     *"+" "+"******  "+" "+" ***** ");
+		System.out.println("*     *"+" "+"*     *"+" "+"*       "+" "+"      *");
+		System.out.println("*     *"+" "+"*     *"+" "+"*       "+" "+"*     *");
+		System.out.println(" ***** "+" "+" ***** "+" "+"*       "+" "+" ***** ");
+	}
+}
+
+
+
+
